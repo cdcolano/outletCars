60033a3f3ffd64e66fd2dcfae9124a920359d711
cdcolano <carlos.delcampo@opendeusto.es>
2021-11-08 18:29:44 +0100

    eliminacion de carpetas fuera del proyecto

--- mysite/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import Empresa, Trabajador
-# Register your models here.
-admin.site.register(Empresa)
-admin.site.register(Trabajador)
