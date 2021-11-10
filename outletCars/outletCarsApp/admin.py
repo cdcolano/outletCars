aad5931cb887c1da609d9d59e22056699df268bc
ander-ruizdeveye <ander.ruizdeveye@opendeusto.es>
2021-11-10 16:41:42 +0100

    integracion

---
+++ outletCars/outletCarsApp/admin.py
@@ -0,0 +1,7 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Categoría, Coche, Marca
+# Register your models here.
+admin.site.register(Categoría)
+admin.site.register(Coche)
+admin.site.register(Marca)
