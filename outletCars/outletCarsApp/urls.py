a33e89af8af0a3a53c2e9b740a023626f24532cf
cdcolano <carlos.delcampo@opendeusto.es>
2021-11-10 20:02:06 +0100

    subidaFinal

--- outletCars/outletCarsApp/urls.py
+++ outletCars/outletCarsApp/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     
 
-    path('listaCategorias/',views.CategoriaListView.as_view(), name="categoria_list_view"),
+    path('listaCategorias',views.CategoriaListView.as_view(), name="categoria_list_view"),
     path('categoria/<int:id_categoria>',views.CategoríaDetailView.as_view(), name="detalle_categoria"),
     path('listaCoches/',views.CocheListView.as_view(), name="coche_list_view"),
     path('coche/<str:matricula>',views.CocheDetailView.as_view(), name="detalle_coche"),
-    path('listaMarcas/',views.ListaMarcasListView.as_view(), name="marca_list_view"),
-    path('marca/<int:marca_id>',views.MarcaDetailView.as_view(), name= "detalle_marca")
+    path('marcas',views.ListaMarcasListView.as_view(), name="marca_list_view"),
+    path('marcas/<int:id_marca>',views.MarcaDetailView.as_view(), name= "detalle_marca")
 
 
 
