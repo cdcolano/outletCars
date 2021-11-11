717570f61266f7deac8fac03bd694510e8d04336
cdcolano <carlos.delcampo@opendeusto.es>
2021-11-11 08:57:51 +0100

    listas con enlaces a detalles

--- outletCars/outletCarsApp/urls.py
+++ outletCars/outletCarsApp/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     
 
-    path('listaCategorias',views.CategoriaListView.as_view(), name="categoria_list_view"),
-    path('categoria/<int:id_categoria>',views.CategoríaDetailView.as_view(), name="detalle_categoria"),
-    path('listaCoches/',views.CocheListView.as_view(), name="coche_list_view"),
-    path('coche/<str:matricula>',views.CocheDetailView.as_view(), name="detalle_coche"),
+    path('categorias',views.CategoriaListView.as_view(), name="categoria_list_view"),
+    path('categorias/<int:id_categoria>',views.CategoríaDetailView.as_view(), name="detalle_categoria"),
+    path('coches',views.CocheListView.as_view(), name="coche_list_view"),
+    path('coches/<str:matricula>',views.CocheDetailView.as_view(), name="detalle_coche"),
     path('marcas',views.ListaMarcasListView.as_view(), name="marca_list_view"),
     path('marcas/<int:id_marca>',views.MarcaDetailView.as_view(), name= "detalle_marca")
 
